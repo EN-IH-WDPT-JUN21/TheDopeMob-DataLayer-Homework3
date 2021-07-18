62ad03282e1234839b3dd0bb63cc0ebd7cd442f1
Jegor Chomczuk <jegor.chomczuk@gmail.com>
2021-07-18 19:19:13 +0200

    New classes: ContactInfo, Lead and Contact

---
+++ src/main/java/Lead/Lead.java
@@ -0,0 +1,19 @@
+package Lead;
+
+import Contact.Contact;
+import ContactInfo.ContactInfo;
+
+public class Lead extends ContactInfo {
+
+    public Lead(String name, String phoneNumber, String email, String companyName) {
+        super(name, phoneNumber, email, companyName);
+    }
+
+    // Methods
+    // Must be TESTED
+    public void convertLead(Lead lead){
+        Contact contact = new Contact(lead.getName(), lead.getPhoneNumber(),lead.getEmail(), lead.getCompanyName());
+// Method to add Contact to ArrayList<Contact> contactList
+        System.out.println("Enter " + contact.getName() + "'s product of interest: Hybrid Truck, Flatbed Truck, or Box Truck");
+    }
+}
