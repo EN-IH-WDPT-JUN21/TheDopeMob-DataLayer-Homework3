df168101ca524276c0f1db9476ed314ec189de0a
Jegor Chomczuk <jegor.chomczuk@gmail.com>
2021-09-01 21:13:17 +0200

    Contact class and Lead class refactor. Plus removing ContactInfo class and ContactInfoTest

--- src/main/java/com/ironhack/dao/Contact.java
+++ src/main/java/com/ironhack/dao/Contact.java
@@ -1,10 +1,37 @@
 package com.ironhack.dao;
 
-import com.ironhack.contact.ContactInfo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public class Contact extends ContactInfo {
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contact_id")
+    private int id;
+
+    private String name;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    private String email;
+
+    @Column(name = "company_name")
+    private String companyName;
 
     public Contact(String name, String phoneNumber, String email, String companyName) {
-        super(name, phoneNumber, email, companyName);
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.companyName = companyName;
     }
 }
--- src/main/java/com/ironhack/dao/Lead.java
+++ src/main/java/com/ironhack/dao/Lead.java
@@ -1,11 +1,37 @@
 package com.ironhack.dao;
 
-import com.ironhack.contact.ContactInfo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public class Lead extends ContactInfo {
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Lead {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contact_id")
+    private int id;
+
+    private String name;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    private String email;
+
+    @Column(name = "company_name")
+    private String companyName;
 
     public Lead(String name, String phoneNumber, String email, String companyName) {
-        super(name, phoneNumber, email, companyName);
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.companyName = companyName;
     }
-
 }
