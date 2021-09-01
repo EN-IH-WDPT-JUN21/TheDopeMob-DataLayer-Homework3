b4b2b07fd92c8e1d48604e050f06ce5edb6fe676
Jegor Chomczuk <jegor.chomczuk@gmail.com>
2021-09-01 22:47:32 +0200

    Broken Lead table during app build to fix

--- src/main/java/com/ironhack/repository/ContactRepository.java
+++ src/main/java/com/ironhack/repository/ContactRepository.java
@@ -1,4 +1,10 @@
 package com.ironhack.repository;
 
-public interface ContactRepository {
+import com.ironhack.dao.Account;
+import com.ironhack.dao.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
 }
