f09e4e821bef26e27cc868e08d74f974f4898211
Gustavo Maldonado <maldonadogustavo55@gmail.com>
2021-09-03 10:42:14 +0200

    Update README with logo

--- src/main/java/com/ironhack/CRMApplication.java
+++ src/main/java/com/ironhack/CRMApplication.java
@@ -1,5 +1,6 @@
 package com.ironhack;
 
+import com.ironhack.data.InputHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class CRMApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CRMApplication.class, args);
+        InputHandler.start();
 
     }
 
